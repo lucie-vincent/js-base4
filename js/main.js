9b1c039559b1c954c42921251fac465129eac9d8
Lucie Vincent <lucie.sarah.vincent@gmail.com>
2024-02-02 10:01:13 +0100

    rectifications: event sur le logo ciblé + background color du container

--- js/main.js
+++ js/main.js
@@ -2,12 +2,15 @@ const container     = document.querySelector("#container");
 const logos         = document.querySelectorAll(".logo");
 const brandNames    = document.querySelectorAll(".brand-name");
 
-
+// on itère pour chaque logo parmis tous les logos
 for (let logo of logos) {
-    logo/addEventListener("click", () => {
+    // on ajoute l'écouteur d'événement sur le clic + une function
+    logo.addEventListener("click", () => {
+        // pour le logo ciblé à ce moment dans l'itération
+        // on alterne entre la classe logo et la classe logo-clicked
         logo.classList.toggle("logo-clicked");
-        for (brandName of brandNames) {
-            brandName.style.display = "block";
-        }
+        // au backgroundColor du container, on attribut, pour le logo ciblé, la couleur mise en data-
+        // grace au dataset
+        container.style.backgroundColor = logo.dataset.color // dataset : récupére le data- dans le html
     })
 }
